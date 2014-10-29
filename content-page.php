846c8e041b938429a650fc699aaec5017fe45a23
ehjung <jesther0103@gmail.com>
2014-10-28 22:42:27 -0700

    Stretch section to fit page

--- content-page.php
+++ content-page.php
@@ -5,15 +5,25 @@
  * @package single-page_theme
  */
 ?>
-
+	
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div id="home_section" class="section">
-		Hommmeee
+		<div class="section_content">
+			Home Section
+		</div>
+		<div class="home_section_bottom_border">
+			Home section bottom border
+		</div>
 	</div>
 
-	<div class="content-section">
+	<div id="about_me_section" class="section">
+		<div class="section_content">
+			About Me Section
+		</div>
+		<div class="about_me_section_bottom_border">
+			About me section bottom border
+		</div>
 		<header class="entry-header">
-			<a id="about_me_section">About Me</a>
 			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		</header><!-- .entry-header -->
 
@@ -23,26 +33,18 @@
 		<div class="section_content">
 			The section should go here
 		</div>
-		<div id="projects_section_bottom_border">
+		<div class="projects_section_bottom_border">
 			The border should go here
 		</div>
 	</div>
 
-	<div class="content-section">
-		<a id="contact_me_section">Contact Me</a>
+	<div id="contact_me_section" class="section">
+		<div class="section">
+			Contact me section
+		</div>
+		<div class="contact_me_section_bottom_border">
+			The border goes here
+		</div>
 	</div>
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'single-page_theme' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php edit_post_link( __( 'Edit', 'single-page_theme' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
+	
 </article><!-- #post-## -->
